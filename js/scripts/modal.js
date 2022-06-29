1b266f2d2aabb5b4345d74c1f9390a1ce56e0857
anderson-rodriguesdev <andersonrodrigues.silva96@gmail.com>
2022-06-29 11:20:18 -0300

    modal login

---
+++ js/scripts/modal.js
@@ -0,0 +1,18 @@
+{
+  const btnOpenModal = document.querySelector('.js-open-modal');
+  const btnCloseModal = document.querySelector('.js-close-modal');
+  const overlay = document.querySelector('.js-overlay');
+
+  btnOpenModal.addEventListener('click', (event) => {
+    event.preventDefault();
+    document.documentElement.classList.add('show-modal');
+  });
+
+  btnCloseModal.addEventListener('click', () => {
+    document.documentElement.classList.remove('show-modal');
+  });
+
+  overlay.addEventListener('click', () => {
+    document.documentElement.classList.remove('show-modal');
+  });
+}
