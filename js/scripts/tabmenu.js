0258d92c577f09dcf5dcc5bc3ae2f75f1abe5b52
anderson-rodriguesdev <andersonrodrigues.silva96@gmail.com>
2022-06-29 22:08:44 -0300

    alteração no tabmenu.js - Como adicionar toggle ao botão sem ser afetado pela função clean?

--- js/scripts/tabmenu.js
+++ js/scripts/tabmenu.js
@@ -3,50 +3,31 @@
   const menuSite = document.querySelectorAll('.js-menu');
 
   function clean() {
+    btnMenu.forEach((btn) => {
+      btn.classList.remove('active');
+    });
     menuSite.forEach((itemMenu) => {
       itemMenu.classList.remove('active');
       itemMenu.addEventListener('mouseleave', () => {
         itemMenu.classList.remove('active');
-        btnMenu.forEach((btn) => {
-          btn.classList.remove('active');
+        btnMenu.forEach((botao) => {
+          botao.classList.remove('active');
         });
       });
     });
   }
 
-  function abrirMenu(index, btn) {
+  //Como adicionar toggle no botão sem ser afetado pelo loop do clean?
+  function ativarIndex(botao, index) {
     clean();
-    btn.classList.add('active');
+    botao.classList.add('active');
     menuSite[index].classList.add('active');
   }
 
-  btnMenu.forEach((btn, index) => {
-    btn.addEventListener('click', (event) => {
+  btnMenu.forEach((botao, index) => {
+    botao.addEventListener('click', (event) => {
       event.preventDefault();
-      abrirMenu(index, btn);
+      ativarIndex(botao, index);
     });
   });
-
-  // btnMenu.forEach((btn, index) => {
-  //   btn.addEventListener('click', (event) => {
-  //     event.preventDefault();
-
-  //     menuSite.forEach((itemMenu) => {
-  //       itemMenu.classList.remove('active');
-  //       itemMenu.addEventListener('mouseleave', () => {
-  //         itemMenu.classList.remove('active');
-  //         btnMenu.forEach((itemBtn) => {
-  //           itemBtn.classList.remove('active');
-  //         });
-  //       });
-  //     });
-
-  //     btnMenu.forEach((itemBtn) => {
-  //       itemBtn.classList.remove('active');
-  //     });
-
-  //     btn.classList.add('active');
-  //     menuSite[index].classList.add('active');
-  //   });
-  // });
 }
